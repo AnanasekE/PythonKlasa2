1b794afdaa6c1f77036a18fff7a1289e08bb2cc5
AnanasekE <janeksambor@gmail.com>
2022-12-15 11:01:23 +0100

    commit

--- hashingTables/anotherMain.py
+++ hashingTables/anotherMain.py
@@ -3,20 +3,23 @@ def IsAnagram():
     word1 = word[0]
     word2 = word[1]
     letters = {}
+    # dodaje litery z pierwszego słowa do słownika
     for letter in word1:
         letters[letter] = letters.get(letter, 0) + 1
 
+    # odejmuje litery z drugiego słowa od słownika
     for letter in word2:
         if letter in letters:
             letters[letter] -= 1
-        else:
-            letters[letter] = -1
+        # else:
+        # letters[letter] = -1
     for letter in letters:
         if letters[letter] != 0:
             return False
     return True
 
 
+# reszta kodu
 count = int(input())
 values = []
 for _ in range(count):
