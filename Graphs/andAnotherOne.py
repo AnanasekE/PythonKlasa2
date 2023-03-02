ffd4e4e14b6948fb7205db6ec6ec5a96b6b447b7
AnanasekE <janeksambor@gmail.com>
2023-03-02 11:30:02 +0100

    commit

--- Graphs/andAnotherOne.py
+++ Graphs/andAnotherOne.py
@@ -1,35 +1,56 @@
-graph = []
-n = int(input('Enter the number of lines: '))
-for x in range(n):
-    line = input(f'Enter the {x} graph line: ').split(' ')
-    graph.append(line)
+def start():
+    graph1 = []
+    # inputs
+    lineAmount = int(input('Enter the number of lines: '))
 
-print(f'Graph: {graph}')
+    for numberOfWords in range(lineAmount):
+        line = input(f'Enter the {numberOfWords} graph line: ').split(' ')
+        graph1.append(line)
 
-wordsToFind = []
-x = int(input('Enter the number of words to find: '))
-for x in range(x):
-    word = input(f'Enter the {x} word: ')
-    wordsToFind.append(word)
-print(f'Words to find: {wordsToFind}')
+    print(f'Graph: {graph1}')
+
+    words = []
+    numberOfWords = int(input('Enter the number of words to find: '))
+
+    for numberOfWords in range(numberOfWords):
+        word = input(f'Enter the {numberOfWords} word: ')
+        words.append(word)
+
+    print(f'Words to find: {words}')
+    return graph1, words, lineAmount
 
 
-def checkMove(x, y, graph, n):
-    if 0 <= x < len(graph[0]) and 0 <= y < n:
+def checkMove(x, y, visited) -> bool:
+    if (0 <= x < m) and (0 <= y < n) and not visited[y][x]:
         return True
     return False
 
 
+def WalkThroughVertex(x, y, visited, result):
+    result += str(graph[y][x])
+    if result in wordsToFind:
+        print(f'Found word: {result}')
+    visited[y][x] = True
+    adjacent_vertex_indices = [
+        (y + row[i], x + col[i])
+        for i in range(len(row))
+        if checkMove(x + col[i], y + row[i], visited)
+    ]
+    for new_y, new_x in adjacent_vertex_indices:
+        WalkThroughVertex(new_x, new_y, visited, result)
+
+
+graph, wordsToFind, n = start()
+
 m = len(graph[0])
+
+# define available moves
 row = [-1, -1, -1, 0, 0, 1, 1, 1]
 col = [-1, 0, 1, 1, -1, -1, 0, 1]
 
+# start checking
 for x in range(n):
     for y in range(m):
-        moves = [(y + r, x + c) for r in row for c in col]
-        for row, col in moves:
-            if checkMove(col, row, graph, n):
-                # do something
-                print('')
-            else:
-                pass
+        result = ''
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        WalkThroughVertex(x, y, visited, result)
